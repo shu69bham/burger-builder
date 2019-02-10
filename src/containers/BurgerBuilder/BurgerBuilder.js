5ff7ab634d527d9c8b5bb3839012f0d4f76bcc67
shu69bham <shubhamsrivastava746@gmail.com>
2019-02-10 21:17:21 +0530

    done till lecture 156

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
-
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     },
     totalPrice: 4
   };
@@ -15,7 +15,7 @@ class BurgerBuilder extends Component {
     return (
       <>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build controls</div>
+        <BuildControls />
       </>
     );
   }
